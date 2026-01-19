f866757ded0c6b385d8a4553951fe21301ff6eed
ZsoltD <syncler91@gmail.com>
2026-01-19 15:06:22 +0100

    init basic sensor logic

---
+++ Backend/app/sensors/am2302.py
@@ -0,0 +1,23 @@
+# AM2302 / DHT22 Temperature and Humidity Sensor Reader
+
+import time
+import board
+import adafruit_dht
+
+dht = adafruit_dht.DHT22(board.D6, use_pulseio=False)
+
+if __name__ == "__main__":
+
+    while True:
+        try:
+            t = dht.temperature - 1.5
+            h = dht.humidity
+            print(f"{time.strftime("%H:%M:%S", time.localtime())} Hőmérséklet: {t:.1f} °C, Páratartalom: {h:.1f}%")
+        
+        except RuntimeError as e:
+            # print("Olvasási hiba, újrapróbálom…")
+            # print(f"Hiba: {e}")
+            time.sleep(1)        
+            continue
+
+        time.sleep(5)
