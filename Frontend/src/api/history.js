6688fb9c57b48f9ff2ff553b02fe43c789a36b57
ZsoltD <syncler91@gmail.com>
2026-04-20 14:10:41 +0200

    Update Vue dependencies

--- Frontend/src/api/history.js
+++ Frontend/src/api/history.js
@@ -1,5 +1,6 @@
 import { apiClient } from "./client";
 
+// Fetch historical sensor data based on a specified time interval (e.g., last 24 hours)
 export const getHistory = async (timeInterval) => {
   const response = await apiClient.get("/history", {
     params: {
